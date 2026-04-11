f10995d9fca92805f897297e5441b8143ee5cac1
Naga Harish <harinaga20@gmail.com>
2026-04-11 12:28:07 +0530

    fixed 404

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,7 @@ import { setupSockets } from './socket/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const clientDistPath = path.join(__dirname, '..', 'client', 'dist');
+const clientDistPath = path.join(__dirname, '..', '..', 'client', 'dist');
 
 const app = express();
 const httpServer = createServer(app);
